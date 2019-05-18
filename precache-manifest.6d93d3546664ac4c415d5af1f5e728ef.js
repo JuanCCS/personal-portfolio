6e8c7d1ee4404d1842d5893ee3dacd6a17fc12cc
Juan Cachafeiro <juansaenz.work@gmail.com>
2019-05-17 19:48:50 -0500

    Updates

---
+++ precache-manifest.6d93d3546664ac4c415d5af1f5e728ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5936fa0a4f9ac6282ee",
+    "url": "/personal-portfolio/static/css/main.e5c191cb.chunk.css"
+  },
+  {
+    "revision": "d5936fa0a4f9ac6282ee",
+    "url": "/personal-portfolio/static/js/main.a2203480.chunk.js"
+  },
+  {
+    "revision": "199df5cd20e1a679bb0f",
+    "url": "/personal-portfolio/static/js/runtime~main.8fd806ec.js"
+  },
+  {
+    "revision": "2200967545f5b8eaa6af",
+    "url": "/personal-portfolio/static/js/2.e90101c3.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d8d8f444a131854a7d70c9518209294a",
+    "url": "/personal-portfolio/index.html"
+  }
+];
